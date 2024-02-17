e5229873563f2ef9d9698363c63f26435eda1c5e
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-02-17 15:27:56 +0900

    feat: アプリのUIを作成する (#1)
    * アプリタイトル、身長・体重テキストフィールド、計算ボタンの作成
    * 計算結果テキストの作成

--- app/src/main/java/com/example/bmicalcapp/MainActivity.kt
+++ app/src/main/java/com/example/bmicalcapp/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.bmicalcapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.bmicalcapp.ui.theme.BMICalcAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +37,73 @@ class MainActivity : ComponentActivity() {
             BMICalcAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
+                    Column(
+                        horizontalAlignment = Alignment.Start, modifier = Modifier.padding(20.dp)
+                    ) {
+                        Text(
+                            text = "BMI計算アプリ",
+                            fontSize = 36.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "身長(cm)",
+                            color = Color.Blue,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = TextFieldDefaults.colors(
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent,
+                            ),
+                            value = "",
+                            onValueChange = {},
+                            placeholder = { Text(text = "170") },
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            text = "体重(kg)",
+                            color = Color.Blue,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        TextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            colors = TextFieldDefaults.colors(
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent,
+                            ),
+                            value = "",
+                            onValueChange = {},
+                            placeholder = { Text(text = "60") },
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            Text(
+                                text = "計算する",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Center,
+                                color = Color.White,
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center,
+                            text = "あなたのBMIは00.0です",
+                            color = Color.Gray,
+                            fontSize = 24.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+                    }
                 }
             }
         }
